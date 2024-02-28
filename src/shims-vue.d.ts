1ecb77a48674a2a70227d071676275474d1e4076
YSASM <1613921123@qq.com>
2024-02-28 16:49:28 +0800

    update

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,7 +2,11 @@ declare module '*.vue' { //declare声明宣告， 声明一个ambient module(即
     import Vue from 'vue'
     export default Vue
 }
+
 declare module 'vue-clipboard3'
 declare module 'element-plus/dist/locale/zh-cn.mjs'
-declare module 'monaco-editor'
-declare module 'vue-json-viewer'
+declare module 'monaco-editor' {
+    export {
+        editor
+    }
+}
