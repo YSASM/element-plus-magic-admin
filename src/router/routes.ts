f324921d164b27fd0c10ca90c94135bfa87e33a9
YSASM <1613921123@qq.com>
2024-02-29 13:37:15 +0800

    O.o

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,113 +1,113 @@
-import type {RouteRecordRaw} from "vue-router"
+import type { RouteRecordRaw } from "vue-router"
 import TableLayout from '../layout/TableLayout.vue'
-const routes:Array<RouteRecordRaw>=[
-    {
-      meta: {
-        hidden: true
-      },
-      path: '/login',
-      component: import("../views/LoginPage.vue"),
-      children: []
+const routes: Array<RouteRecordRaw> = [
+  {
+    meta: {
+      hidden: true
     },
-    {
-      path: '/',
-      redirect: '/home',
-      children: [
-        {
-          path: "home",
-          component: import("@/views/HomeView.vue"),
-          meta: {
-            title: '首页',
-            icon: 'House',
-          },
-        }
-      ]
-    },
-    {
-      path: '/user',
-      meta: {
-        title: '用户管理',
-        icon: 'Avatar',
-      },
-      children: [
-        {
-          path: "list",
-          component: TableLayout,
-          meta: {
-            title: '用户列表',
-            icon: 'User',
-          },
+    path: '/login',
+    component: import("../views/LoginPage.vue"),
+    children: []
+  },
+  {
+    path: '/',
+    redirect: '/home',
+    children: [
+      {
+        path: "home",
+        component: import("@/views/HomeView.vue"),
+        meta: {
+          title: '首页',
+          icon: 'House',
         },
-        {
-          path: "event",
-          component: TableLayout,
-          meta: {
-            title: '用户日志',
-            icon: 'Clock',
-          },
+      }
+    ]
+  },
+  {
+    path: '/user',
+    meta: {
+      title: '用户管理',
+      icon: 'Avatar',
+    },
+    children: [
+      {
+        path: "list",
+        component: TableLayout,
+        meta: {
+          title: '用户列表',
+          icon: 'User',
         },
-        {
-          path: 'pushlog',
-          name: 'pushlog',
-          component: TableLayout,
-          meta: {
-            title: '推送日志',
-            icon: 'Promotion',
-          },
+      },
+      {
+        path: "event",
+        component: TableLayout,
+        meta: {
+          title: '用户日志',
+          icon: 'Clock',
         },
-      ]
-    },
-    {
-      meta: {
-        title: '商品管理',
-        icon: 'ShoppingCart',
       },
-      path: '/goods',
-      children: [
-        {
-          path: 'goods',
-          component: TableLayout,
-          meta: {
-            title: '商品管理',
-            icon: 'Goods',
-          },
-        }
-      ],
-    },
-    {
-      meta: {
-        title: '订单管理',
-        icon: 'Money',
+      {
+        path: 'pushlog',
+        name: 'pushlog',
+        component: TableLayout,
+        meta: {
+          title: '推送日志',
+          icon: 'Promotion',
+        },
       },
-      path: '/order',
-      children: [
-        {
-          path: 'order',
-          component: TableLayout,
-          meta: {
-            title: '订单管理',
-            icon: 'Wallet',
-          },
-        }
-      ],
+    ]
+  },
+  {
+    meta: {
+      title: '商品管理',
+      icon: 'ShoppingCart',
     },
-    {
-      path: '/config',
-      meta: {
-        title: '配置管理',
-        icon: 'Setting',
-      },
-      children: [
-        {
-          path: "config",
-          component: TableLayout,
-          meta: {
-            title: '配置列表',
-            icon: 'Filter',
-          },
-        }
-      ]
+    path: '/goods',
+    children: [
+      {
+        path: 'goods',
+        component: TableLayout,
+        meta: {
+          title: '商品管理',
+          icon: 'Goods',
+        },
+      }
+    ],
+  },
+  {
+    meta: {
+      title: '订单管理',
+      icon: 'Money',
     },
-  ]
+    path: '/order',
+    children: [
+      {
+        path: 'order',
+        component: TableLayout,
+        meta: {
+          title: '订单管理',
+          icon: 'Wallet',
+        },
+      }
+    ],
+  },
+  {
+    path: '/config',
+    meta: {
+      title: '配置管理',
+      icon: 'Setting',
+    },
+    children: [
+      {
+        path: "config",
+        component: TableLayout,
+        meta: {
+          title: '配置列表',
+          icon: 'Filter',
+        },
+      }
+    ]
+  },
+]
 
 export default routes
--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,73 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // vs code 编辑器配置
-import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+// import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   build: {
-    chunkSizeWarningLimit:5000,
+    emptyOutDir: true, // 打包时先清空上一次构建生成的目录
+    // outDir: "distp", //指定输出路径
+    // assetsDir: "static/img/", // 指定生成静态资源的存放路径
+    chunkSizeWarningLimit: 5000, // 消除打包大小超过500kb警告
+    minify: "terser", // Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效
+    terserOptions: {
+      compress: {
+        keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+        // drop_console: true, // 生产环境去除 console
+        // drop_debugger: true, // 生产环境去除 debugger
+      },
+      // format: {
+      //   comments: false, // 删除注释
+      // },
+    },
     rollupOptions: {
+      //体积太大，模块拆分
       output: {
-        // 分包
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
-        }
-      }
-    }
+        },
+        // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+        entryFileNames: "static/js/[name]-[hash].js",
+        
+        // 用于命名代码拆分时创建的共享块的输出命名
+        chunkFileNames: "static/js/[name]-[hash].js", //js文件放到一个文件夹
+        // chunkFileNames: (chunkInfo) => { // 拆分js到对应模块文件夹
+        //   const facadeModuleId = chunkInfo.facadeModuleId
+        //     ? chunkInfo.facadeModuleId.split("/")
+        //     : [];
+        //   const fileName =
+        //     facadeModuleId[facadeModuleId.length - 2] || "[name]";
+        //   return `static/js/${fileName}/[name].[hash].js`;
+        // },
+
+        // 用于输出静态资源的命名，[ext]表示文件扩展名
+        // assetFileNames: "static/[ext]/[name]-[hash].[ext]",  //根据拓展名拆分文件夹
+        assetFileNames: (assetInfo) => {
+          //根据类型拆分文件夹
+          const info = assetInfo.name!.split(".");
+          let extType = info[info.length - 1];
+          if (
+            /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name!)
+          ) {
+            extType = "media";
+          } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name!)) {
+            extType = "img";
+          } else if (
+            /\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name!)
+          ) {
+            extType = "fonts";
+          }
+          return `static/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
   },
-  
   plugins: [
     vue(),
     vueJsx(),
@@ -33,9 +82,9 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    (monacoEditorPlugin as { default?: any }).default({
-      languageWorkers: ['editorWorkerService', 'json']
-    }),
+    // (monacoEditorPlugin as { default?: any }).default({
+    //   languageWorkers: ['json']
+    // }),
   ],
   resolve: {
     alias: {
